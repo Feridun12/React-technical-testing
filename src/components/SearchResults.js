7a9e1d8869727a849a5fdb017c069c5b9a814b47
Feridun <feridun@aerlabs.com>
2023-12-22 11:27:52 +0100

    closes #2

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,24 +1,30 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import "../styles/SearchResults.css"
+import PropTypes from "prop-types";
+import "../styles/SearchResults.css";
 
-function SearchResults({results}) {
-    if (!results.length) {
-        return <p className="result-paraghraph">No results. Please try again later</p>
-      }
-      else {
-  return (
-    <>
-      <div className="image-container">
-        {results.map((image) => (
-          <img className="image" src={image} alt="images of the planet that has been searched example: moon pictures" />
-        ))}
-     </div>
-    </>
-  );
-}}
+function SearchResults({ results }) {
+  if (results.length < 0) {
+    return (
+      <p className="result-paraghraph">No results. Please try again later</p>
+    );
+  } else {
+    return (
+      <>
+        <div className="image-container">
+          {results.map((image) => (
+            <img
+              className="image"
+              src={image}
+              alt="images of the planet that has been searched example: moon pictures"
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
 
 SearchResults.propTypes = {
   results: PropTypes.array.isRequired,
-}
+};
 export default SearchResults;
