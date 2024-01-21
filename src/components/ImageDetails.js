c16d04f6d98b073fd976149a2cadd7e3bea61bcb
Feridun <feridun@aerlabs.com>
2024-01-21 16:24:09 +0200

    cleared out excess comments

--- src/components/ImageDetails.js
+++ src/components/ImageDetails.js
@@ -5,22 +5,8 @@ function ImageDetails() {
   const sessionData = JSON.parse(sessionStorage.getItem("imageData"));
 
   console.log(sessionData, "sessionData");
-  // useEffect(() => {
-  //   console.log("this effect is running");
-  //   // Retrieve the image data from session storage
 
-  //   // Set the image data in the context
-  //   if (imageData) {
-  //     setImageData(imageData);
-  //   }
-  // });
-
-  return (
-    <div className="image-details">
-      Sup boi
-      <div>{sessionData.data[0].center}</div>
-    </div>
-  );
+  return <div className="image-details"></div>;
 }
 
 export default ImageDetails;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,16 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import ScaleLoader from "react-spinners/ScaleLoader";
 import { useNavigate } from "react-router-dom";
-import useImageDataContext from "../custom-hooks/useImageDataContext";
 import "../styles/SearchResults.css";
 
 function SearchResults({ searchResults, validSearch, loading }) {
   const navigate = useNavigate();
-  // we get the state from the context since SearchResults is a children of ContextProvider component
-  // const { setImageData } = useImageDataContext();
 
   const handleImageClick = (id, imageData) => {
-    // storing the image data in the session so a refresh in the ImageDetails wont crash the page
+    // storing the image data in the session so a refresh in the ImageDetails component wont crash the page
     sessionStorage.setItem("imageData", JSON.stringify(imageData));
     navigate(`/images/${id}`);
   };
