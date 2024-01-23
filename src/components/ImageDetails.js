d4e3fad0d9e58664c042366d2510d93e39db71a8
Feridun <feridun@aerlabs.com>
2024-01-23 13:38:34 +0200

    added futuristic font to the image text

--- src/components/ImageDetails.js
+++ src/components/ImageDetails.js
@@ -6,7 +6,24 @@ function ImageDetails() {
 
   console.log(sessionData, "sessionData");
 
-  return <div className="image-details"></div>;
+  return (
+    <div className="image-details">
+      <h1 className="image-title">
+        {/* Title:
+        <br></br> */}
+        {sessionData.data[0].title}
+      </h1>
+      <img
+        // just putting image as a classname felt a bit too broad
+        className="detail-image"
+        src={sessionData.links[0].href}
+        alt={sessionData.data[0].description}
+      />
+      <div className="image-description">
+        <p>{sessionData.data[0].description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ImageDetails;
