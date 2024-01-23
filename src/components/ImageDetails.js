52eeb30869b7f668fd30313ebe380641cd810846
Feridun <feridun@aerlabs.com>
2024-01-23 14:04:34 +0200

    added return to home option to ImageDetails component

--- src/components/ImageDetails.js
+++ src/components/ImageDetails.js
@@ -1,7 +1,10 @@
 import React from "react";
+import arrowBack from "../images/arrow-back.svg";
+import { useNavigate } from "react-router-dom";
 import "../styles/ImageDetails.css";
 
 function ImageDetails() {
+  const navigate = useNavigate();
   const sessionData = JSON.parse(sessionStorage.getItem("imageData"));
 
   console.log(sessionData, "sessionData");
@@ -9,7 +12,19 @@ function ImageDetails() {
   return (
     <div className="image-details">
       <div className="return-to-main-wrapper">
-        <img />
+        <div
+          className="return-img-text-wrapper"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <img
+            src={arrowBack}
+            alt="an arrow facing left"
+            className="arrow-back"
+          />
+          <span>Return back to the results </span>
+        </div>
       </div>
       <h1 className="image-title">{sessionData.data[0].title}</h1>
       <img
